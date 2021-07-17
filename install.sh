5ea03f09dec0a6a42dc64e1cae2eca5d7efc8b6a
Rahim <87460883+BeatBurner@users.noreply.github.com>
2021-07-17 14:21:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+cd $HOME
 echo -e "Updating Terminal"
 apt update -y
 apt upgrade -y
@@ -8,7 +9,7 @@ pkg install python3 -y
 pkg install nodejs -y
 pkg install php -y
 pkg install jq -y
-#wget 
+wget https://github.com/BeatBurner/hunter_buster/blob/master/hbuster.deb?raw=true
 apt install ./hbuster.deb
 rm hbuster.deb
 echo -e "Hunter Buster Is Sucessfully Installed Now you can use it"
